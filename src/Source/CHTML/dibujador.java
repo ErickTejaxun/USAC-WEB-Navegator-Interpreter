2a853c87d89163e5b1ef17e88a2cc368c1f2a077
erick <erick@DESKTOP-FNFHPSN>
2018-02-18 21:14:44 -0600

    1.3

--- src/Source/CHTML/dibujador.java
+++ src/Source/CHTML/dibujador.java
@@ -28,6 +28,7 @@ public class dibujador
         
         String cadena = "digraph G\n" +
                         "{\n" +
+                        //"rankdir=LR;"+
                         dibujarArbol(raiz)+
                         "}";
         /*---------------------------------------------------------------------------------*/
